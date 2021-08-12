c07c9b23e6104e292ea6e61c218b8ceab1be7334
Hanyeop <super7615@naver.com>
2021-08-12 12:16:17 +0900

    ComposeEx Project (2) - 기초 사용법

--- ComposeEx/app/src/main/java/com/hanyeop/composeex/MainActivity.kt
+++ ComposeEx/app/src/main/java/com/hanyeop/composeex/MainActivity.kt
@@ -3,11 +3,21 @@ package com.hanyeop.composeex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hanyeop.composeex.ui.theme.ComposeExTheme
 import com.hanyeop.composeex.ui.theme.Purple200
 
@@ -15,25 +25,73 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeExTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = Purple200) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeExTheme {
+        Surface(color = Purple200) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button( onClick = { updateCount(count+1) },
+            colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (count > 5) Color.Cyan else Color.White)
+    ) {
+        Text("$count 번 클릭하셨네요!")
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = List(100) { "안드로이드 #$it" }) {
+    val counterState = remember { mutableStateOf(0) }
+
+    Column(modifier = Modifier.fillMaxHeight()) {
+        NameList(names, Modifier.weight(1f))
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            }
+        )
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+
+    Text(text = "안녕하세요 $name!",
+        modifier = Modifier
+        .padding(24.dp)
+        .background(color = backgroundColor)
+        .clickable(onClick = { isSelected = !isSelected }))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeExTheme {
-        Greeting("Android")
+    MyApp {
+        MyScreenContent()
     }
 }
