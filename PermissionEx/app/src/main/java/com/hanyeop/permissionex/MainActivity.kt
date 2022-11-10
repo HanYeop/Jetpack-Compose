b6c72b1c484b2de3d9c4a0e00e8bc89fb6f229e7
SangyeopHan <super7615@naver.com>
2022-11-10 19:35:46 +0900

    PermissionEx - done

--- PermissionEx/app/src/main/java/com/hanyeop/permissionex/MainActivity.kt
+++ PermissionEx/app/src/main/java/com/hanyeop/permissionex/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.hanyeop.permissionex
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.hanyeop.permissionex.ui.theme.PermissionExTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +27,73 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PermissionExTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MyApp(){
+    val context = LocalContext.current
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PermissionExTheme {
-        Greeting("Android")
+    /** 요청할 권한 **/
+    val permissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    val launcherMultiplePermissions = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissionsMap ->
+        val areGranted = permissionsMap.values.reduce { acc, next -> acc && next }
+        /** 권한 요청시 동의 했을 경우 **/
+        if (areGranted) {
+            Log.d("test5", "권한이 동의되었습니다.")
+        }
+        /** 권한 요청시 거부 했을 경우 **/
+        else {
+            Log.d("test5", "권한이 거부되었습니다.")
+        }
+    }
+
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Button(onClick = {
+            checkAndRequestPermissions(
+                context,
+                permissions,
+                launcherMultiplePermissions
+            )
+        }) {
+            Text(text = "권한 요청하기")
+        }
     }
 }
+
+fun checkAndRequestPermissions(
+    context: Context,
+    permissions: Array<String>,
+    launcher: ManagedActivityResultLauncher<Array<String>, Map<String, Boolean>>,
+) {
+
+    /** 권한이 이미 있는 경우 **/
+    if (permissions.all {
+            ContextCompat.checkSelfPermission(
+                context,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }) {
+        Log.d("test5", "권한이 이미 존재합니다.")
+    }
+
+    /** 권한이 없는 경우 **/
+    else {
+        launcher.launch(permissions)
+        Log.d("test5", "권한을 요청하였습니다.")
+    }
+}
+
