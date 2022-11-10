4eb2a21ceae9f2e48f13238d2e3b8b6a07a3ebf2
SangyeopHan <super7615@naver.com>
2022-11-10 18:56:19 +0900

    PermissionEx - init

---
+++ PermissionEx/app/src/main/java/com/hanyeop/permissionex/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.hanyeop.permissionex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hanyeop.permissionex.ui.theme.PermissionExTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PermissionExTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PermissionExTheme {
+        Greeting("Android")
+    }
+}
