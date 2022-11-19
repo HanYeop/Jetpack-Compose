a8614173001073684d4500441514fd2094732d4a
SangyeopHan <super7615@naver.com>
2022-11-19 19:50:30 +0900

    WorkManagerEx - PeriodicWorkRequest

--- WorkManagerEx/app/src/main/java/com/hanyeop/workmanagerex/MainActivity.kt
+++ WorkManagerEx/app/src/main/java/com/hanyeop/workmanagerex/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hanyeop.workmanagerex
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,34 +11,78 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import com.hanyeop.workmanagerex.ui.theme.WorkManagerExTheme
+import com.hanyeop.workmanagerex.work.LogWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
+const val UNIQUE_NAME = "LOG"
 class MainActivity : ComponentActivity() {
+
+    companion object{
+        // 주기 설정. 최소 15분
+        private val workRequest: PeriodicWorkRequest =
+            PeriodicWorkRequestBuilder<LogWorker>(15, TimeUnit.MINUTES)
+                .build()
+        lateinit var workManager: WorkManager
+
+        // 워크매니저 시작
+        fun startWorkManager() {
+            workManager.enqueueUniquePeriodicWork(
+                UNIQUE_NAME,
+                ExistingPeriodicWorkPolicy.KEEP, // 충돌 정책 : 기존 작업을 유지하고 새 작업을 무시
+                workRequest
+            )
+
+            // 워크 매니저 상태 확인
+            val state = workManager.getWorkInfosForUniqueWork(UNIQUE_NAME).get()
+            for(i in state){
+                Log.d("test5", "startWorkManager: $state")
+            }
+        }
+
+        // 워크매니저 종료
+        fun stopWorkManager() {
+            workManager.cancelUniqueWork(UNIQUE_NAME)
+
+            // 워크 매니저 상태 확인
+            val state = workManager.getWorkInfosForUniqueWork(UNIQUE_NAME).get()
+            for(i in state){
+                Log.d("test5", "stopWorkManager: $state")
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        workManager = WorkManager.getInstance(applicationContext)
+
+        startWorkManager()
+
+        stopWorkManager()
+
         setContent {
             WorkManagerExTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+private fun MyApp(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WorkManagerExTheme {
-        Greeting("Android")
     }
 }
---
+++ WorkManagerEx/app/src/main/java/com/hanyeop/workmanagerex/work/LogWorker.kt
@@ -0,0 +1,17 @@
+package com.hanyeop.workmanagerex.work
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class LogWorker(appContext: Context, workerParams: WorkerParameters)
+    : Worker(appContext, workerParams) {
+
+    override fun doWork(): Result {
+
+        Log.d("test5", "doWork: 비동기 통신")
+
+        return Result.success()
+    }
+}
